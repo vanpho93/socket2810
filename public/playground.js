a1481101e5c1f2121fe53b721c8cd707ed927f9c
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-26 18:29:28 +0700

    copy

--- public/playground.js
+++ public/playground.js
@@ -1,1 +1,35 @@
-const socket = io();
+/*
+*/
+
+// 1. Set su kien cho button
+$('#btnAdd').click(() => {
+    const str = $('h3').text();
+    $('h3').text(str + '!');
+    // console.log(text);
+    // $('h3').append('!');
+});
+
+// $('#btnSend').click(() => {
+    //     const message = $('#txtMessage').val();
+    //     alert(message);
+    //     $('#txtMessage').val('');
+// });
+
+$('#btnSend').click(() => {
+    // 2. Lay gia tri cua input
+    const message = $('#txtMessage').val();
+    // 3. Set html cho cac DOM
+    $('#ulMessages').append(`<li>${message}</li>`);
+    // $('#ulMessages').html(`<li>${message}</li>`);
+});
+
+// $('#ulMessages li').click(function() {
+//     $('#ulMessages li').removeClass('active');
+//     $(this).addClass('active');
+// });
+
+$('#ulMessages').on('click', 'li', function() {
+//  4. Gan class cho DOM
+    $('#ulMessages li').removeClass('active');
+    $(this).addClass('active');
+});
